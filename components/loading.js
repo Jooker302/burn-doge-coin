e58f1ec70be7005706d399fc3561b3527deda26b
jooker302 <i.omer6637@gmail.com>
2023-08-20 03:54:33 -0700

    Yellow Border

--- components/loading.js
+++ components/loading.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Loader = () => (
   <div className="flex items-center justify-center h-screen bg-gray-100">
-    <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
+    <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-[#FFF730]"></div>
   </div>
 );
 
